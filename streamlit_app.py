c324510a27faee721d21b9eabfec4ee0e1178083
Bruna Almeida Santos <brunadosantosalmeida@gmail.com>
2026-06-30 14:27:44 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ try:
   cnx = st.connection("snowflake")
   session = cnx.session()
 
-  my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+  my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
   st.dataframe(data=my_dataframe, use_container_width=True)
   st.stop()
   
