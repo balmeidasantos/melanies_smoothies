0ad0828b1394aac805f175d4984a108edfef654e
Bruna Almeida Santos <brunadosantosalmeida@gmail.com>
2026-06-30 13:23:42 -0300

    Enhance order submission with error handling
    Added error handling for order submission and improved success message.

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,12 +35,18 @@ try:
       my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                       values ('""" + ingredients_string + """','"""+ name_on_order + """')"""
   
-      st.write(my_insert_stmt)
-      #st.stop()
-      time_to_insert = st.button("Submit Order")
-      if time_to_insert:
-          session.sql(my_insert_stmt).collect()
-          st.success('Your Smoothie is ordered!', icon="✅")
+      # Button to submit order
+        time_to_insert = st.button('Submit Order')
+        if time_to_insert:
+            try:
+                # Execute SQL insert statement
+                session.sql(my_insert_stmt).collect()
+                st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+            except Exception as e:
+                st.error(f"Failed to submit order: {str(e)}")
+        
+except Exception as ex:
+    st.error(f"An error occurred: {str(ex)}")
       
 # Display a link
 st.write("https://github.com/brunadsa")
